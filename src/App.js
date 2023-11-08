951e587b7ad210c9fed85d0ead99329b79cdfe66
JohnB <jhondark@gmail.com>
2023-11-08 15:10:33 -0500

    update hello world

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react"
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = "John"
+  return <div>Hello {value}</div>
 }
 
-export default App;
+export default App
