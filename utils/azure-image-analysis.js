159ab24e9d61cf45cff9d7ac939d2636801e16d4
JohnB <jhondark@gmail.com>
2023-11-09 10:34:44 -0500

    update analyzeImage function #2

--- utils/azure-image-analysis.js
+++ utils/azure-image-analysis.js
@@ -1,6 +1,6 @@
 const analyzeImage = async (image) => {
   const params = ["objects", "caption", "tags", "denseCaptions", "read"]
-  const isProduction = process.env.NODE_ENV === "production"
+  const isProduction = process.env.NODE_ENV === "Production"
   const visionEndpoint = isProduction
     ? process.env.VITE_VISION_ENDPOINT
     : import.meta.env.VITE_VISION_ENDPOINT
